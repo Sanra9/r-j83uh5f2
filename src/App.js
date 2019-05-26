8803211fd481f4aef041b6cc64cb7ae3a394c77a
sanra9 <sanrra999@gmail.com>
2019-05-26 15:27:18 -0500

    jé

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      texto: '',
+      count: 0,
+    }
+  }
+
   render() {
+    const { texto, count } = this.state
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.textCount.bind(this)}></textarea>
+        <div className="counter">{count}</div>
       </div>
     );
   }
+
+  textCount(event){
+    this.setState({
+      count: event.target.value.length
+    })
+  }
+
 }
 
 export default App;
